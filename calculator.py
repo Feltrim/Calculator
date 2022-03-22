f2838e6fc3d62102bcd8cb3f477bcf2a46d05c6a
Vitor Feltrim <99485226+Feltrim@users.noreply.github.com>
2022-03-22 00:52:27 -0300

    Code

---
+++ calculator.py
@@ -0,0 +1,27 @@
+from lib.dados import *
+
+header('Python Calculator')
+
+n1 = read_int('Digit the first number\n>>>>> ')
+n2 = read_int('Digit the second number\n>>>>> ')
+
+
+while True:
+    opt = menu(['Addition', 'Subtraction', 'Multiplication', 'Division', 'Select new numbers', 'Close the Program'])
+    if opt == 1:
+        add(n1, n2)
+    elif opt == 2:
+        sub(n1, n2)
+    elif opt == 3:
+        mult(n1, n2)
+    elif opt == 4:
+        div(n1, n2)
+    elif opt == 5:
+        n1 = read_int('Digit the first number\n>>>>> ')
+        n2 = read_int('Digit the second number\n>>>>> ')
+    elif opt == 6:
+        header('''Thanks for using my program!!
+Developed by:\033[1;96m Feltrim\033[m''')
+        break
+    else:
+        print('\033[31mPlease, select a valid option.\033[m')
---
+++ lib/operations/__init__.py
@@ -0,0 +1,21 @@
+from calculadora.lib.interface import *
+
+
+def add(a=0, b=0):
+    res = a + b
+    return header(f'The result of\033[1;33m {a} + {b}\033[m = \033[1;34m{res:.2f}\033[m')
+
+
+def sub(a=0, b=0):
+    res = a - b
+    return header(f'The result of\033[1;33m {a} - {b}\033[m = \033[1;34m{res:.2f}\033[m')
+
+
+def mult(a=0, b=0):
+    res = a * b
+    return header(f'The result of\033[1;33m {a} x {b}\033[m = \033[1;34m{res:.2f}\033[m')
+
+
+def div(a=0, b=0):
+    res = a / b
+    return header(f'The result of\033[1;33m {a} ÷ {b}\033[m = \033[1;34m{res:.2f}\033[m')
