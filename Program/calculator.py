c28a5abf68ecbbbde3a6799b43e82aba56a6618e
Vitor Feltrim <99485226+Feltrim@users.noreply.github.com>
2022-04-15 13:37:28 -0300

    Made some changes
    Added some operations and some layout changes

--- Program/calculator.py
+++ Program/calculator.py
@@ -1,25 +1,44 @@
 from lib.operations import *
+from time import sleep
 
 header('Python Calculator')
 
-n1 = read_int('Digit the first number\n>>>>> ')
-n2 = read_int('Digit the second number\n>>>>> ')
-
-
 while True:
-    opt = menu(['Addition', 'Subtraction', 'Multiplication', 'Division', 'Select new numbers', 'Close the Program'])
-    if opt == 1:
+    n1 = read_int('Enter an integer\n>>>>> ')
+    opt1 = menu(['Addition', 'Subtraction', 'Multiplication', 'Division', 'More Options', 'Close the Program'])
+    if opt1 == 1:
+        n2 = read_int(f'>>>>>{n1} + ')
         add(n1, n2)
-    elif opt == 2:
+    elif opt1 == 2:
+        n2 = read_int(f'>>>>>{n1} - ')
         sub(n1, n2)
-    elif opt == 3:
+    elif opt1 == 3:
+        n2 = read_int(f'>>>>>{n1} x ')
         mult(n1, n2)
-    elif opt == 4:
+    elif opt1 == 4:
+        n2 = read_int(f'>>>>>{n1} ÷ ')
         div(n1, n2)
-    elif opt == 5:
-        n1 = read_int('Digit the first number\n>>>>> ')
-        n2 = read_int('Digit the second number\n>>>>> ')
-    elif opt == 6:
+    elif opt1 == 5:
+        opt2 = menu(['Exponentiation', 'Square Root', 'Factorial', 'Close the Program'])
+        if opt2 == 1:
+            n2 = read_int(f'>>>>>{n1}^')
+            exp(n1, n2)
+        elif opt2 == 2:
+            sqrtt(n1)
+        elif opt2 == 3:
+            fac(n1)
+        elif opt2 == 4:
+            header('Finishing....')
+            sleep(1.5)
+            header('''Thanks for using my program!!
+Developed by:\033[1;96m Feltrim\033[m''')
+            break
+        else:
+            print('\033[31mPlease, select a valid option.\033[m')
+
+    elif opt1 == 6:
+        header('Finishing....')
+        sleep(1.5)
         header('''Thanks for using my program!!
 Developed by:\033[1;96m Feltrim\033[m''')
         break
--- Program/lib/operations/__init__.py
+++ Program/lib/operations/__init__.py
@@ -1,24 +1,41 @@
+from pure_eval import is_expression_interesting
 from lib.interface import *
+from math import factorial, sqrt
 
 
 def add(a=0, b=0):
     res = a + b
-    return header(f'The result of\033[1;33m {a} + {b}\033[m = \033[1;34m{res:.0f}\033[m')
+    return header(f'>>>>>\033[1;33m{a} + {b}\033[m = \033[1;34m{res:.0f}\033[m')
 
 
 def sub(a=0, b=0):
     res = a - b
-    return header(f'The result of\033[1;33m {a} - {b}\033[m = \033[1;34m{res:.0f}\033[m')
+    return header(f'>>>>>\033[1;33m{a} - {b}\033[m = \033[1;34m{res:.0f}\033[m')
 
 
 def mult(a=0, b=0):
     res = a * b
-    return header(f'The result of\033[1;33m {a} x {b}\033[m = \033[1;34m{res:.0f}\033[m')
+    return header(f'>>>>>\033[1;33m{a} x {b}\033[m = \033[1;34m{res:.0f}\033[m')
 
 
 def div(a=0, b=0):
     res = a / b
     if a % b == 0:
-        return header(f'The result of\033[1;33m {a} ÷ {b}\033[m = \033[1;34m{res:.0f}\033[m')
+        return header(f'\033[1;33m{a} ÷ {b}\033[m = \033[1;34m{res:.0f}\033[m')
     else:
-        return header(f'The result of\033[1;33m {a} ÷ {b}\033[m = \033[1;34m{res:.2f}\033[m')
+        return header(f'\033[1;33m{a} ÷ {b}\033[m = \033[1;34m{res:.2f}\033[m')
+
+def exp(a=0, b=0):
+    res = a ** b
+    return header(f'\033[1;33m{a}^{b}\033[m = \033[1;34m{res:.0f}\033[m')
+
+def sqrtt(a=0):
+    res = sqrt(a)
+    if res.is_integer():
+        return header(f'\033[1;33m√{a}\033[m = \033[1;34m{res:.0f}\033[m')
+    else:
+        return header(f'\033[1;33m√{a}\033[m = \033[1;34m{res:.5f}\033[m')
+
+def fac(a):
+    res = factorial(a)
+    return header(f'\033[1;33m{a}!\033[m = \033[1;34m{res:.0f}\033[m')
