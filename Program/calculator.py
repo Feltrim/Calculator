9ac129229dafef7f0362645f545e31f8116d19d1
Vitor Feltrim <99485226+Feltrim@users.noreply.github.com>
2022-04-26 14:51:24 -0300

    Fixed MORE visual bugs

--- Program/calculator.py
+++ Program/calculator.py
@@ -7,21 +7,21 @@ while True:
     n1 = read_int('Enter an integer\n>>>>> ')
     opt1 = menu(['Addition', 'Subtraction', 'Multiplication', 'Division', 'More Options', 'Close the Program'])
     if opt1 == 1:
-        n2 = read_int(f'>>>>>{n1} + ')
+        n2 = read_int(f'>>>>> {n1} + ')
         add(n1, n2)
     elif opt1 == 2:
-        n2 = read_int(f'>>>>>{n1} - ')
+        n2 = read_int(f'>>>>> {n1} - ')
         sub(n1, n2)
     elif opt1 == 3:
-        n2 = read_int(f'>>>>>{n1} x ')
+        n2 = read_int(f'>>>>> {n1} x ')
         mult(n1, n2)
     elif opt1 == 4:
-        n2 = read_int(f'>>>>>{n1} ÷ ')
+        n2 = read_int(f'>>>>> {n1} ÷ ')
         div(n1, n2)
     elif opt1 == 5:
         opt2 = menu(['Exponentiation', 'Square Root', 'Factorial', 'Close the Program'])
         if opt2 == 1:
-            n2 = read_int(f'>>>>>{n1}^')
+            n2 = read_int(f'>>>>> {n1}^')
             exp(n1, n2)
         elif opt2 == 2:
             sqrtt(n1)
