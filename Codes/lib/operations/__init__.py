f204e0cd17b48effac539343fdd82edb1c18cb41
Vitor Feltrim <99485226+Feltrim@users.noreply.github.com>
2022-04-27 19:38:28 -0300

    Update Division operation
    Fixed ZeroDivisionError

--- Codes/lib/operations/__init__.py
+++ Codes/lib/operations/__init__.py
@@ -1,3 +1,4 @@
+from zmq import Again
 from lib.interface import *
 from math import factorial, sqrt
 
@@ -18,11 +19,21 @@ def mult(a=0, b=0):
 
 
 def div(a=0, b=0):
-    res = a / b
-    if res.is_integer():
-        return header(f'>>>>> \033[1;33m{a} ÷ {b}\033[m = \033[1;34m{res:.0f}\033[m')
-    else:
-        return header(f'>>>>> \033[1;33m{a} ÷ {b}\033[m = \033[1;34m{res}\033[m')
+    try:
+        res = a / b
+    except ZeroDivisionError:
+        while True:
+            print('\033[31mERROR: A number can not be divided by 0 (zero).\033[m')
+            b = read_int(f'>>>>> {a} ÷ ')
+            if b == 0:
+                continue
+            else:
+                res = a / b
+                if res.is_integer():
+                    return header(f'>>>>> \033[1;33m{a} ÷ {b}\033[m = \033[1;34m{res:.0f}\033[m')
+                else:
+                    return header(f'>>>>> \033[1;33m{a} ÷ {b}\033[m = \033[1;34m{res}\033[m')
+
 
 def exp(a=0, b=0):
     res = a ** b
