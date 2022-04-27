5a66850f4866ef12f61c8b4df05cb9602aa97a62
Vitor Feltrim <99485226+Feltrim@users.noreply.github.com>
2022-04-27 12:21:19 -0300

    Update operations
    This was the last commit I swear

--- Codes/lib/operations/__init__.py
+++ Codes/lib/operations/__init__.py
@@ -4,29 +4,29 @@ from math import factorial, sqrt
 
 def add(a=0, b=0):
     res = a + b
-    return header(f'>>>>> \033[1;33m{a} + {b}\033[m = \033[1;34m{res:.0f}\033[m')
+    return header(f'>>>>> \033[1;33m{a} + {b}\033[m = \033[1;34m{res}\033[m')
 
 
 def sub(a=0, b=0):
     res = a - b
-    return header(f'>>>>> \033[1;33m{a} - {b}\033[m = \033[1;34m{res:.0f}\033[m')
+    return header(f'>>>>> \033[1;33m{a} - {b}\033[m = \033[1;34m{res}\033[m')
 
 
 def mult(a=0, b=0):
     res = a * b
-    return header(f'>>>>> \033[1;33m{a} x {b}\033[m = \033[1;34m{res:.0f}\033[m')
+    return header(f'>>>>> \033[1;33m{a} x {b}\033[m = \033[1;34m{res}\033[m')
 
 
 def div(a=0, b=0):
     res = a / b
-    if a % b == 0:
+    if res.is_integer():
         return header(f'>>>>> \033[1;33m{a} ÷ {b}\033[m = \033[1;34m{res:.0f}\033[m')
     else:
-        return header(f'>>>>> \033[1;33m{a} ÷ {b}\033[m = \033[1;34m{res:.2f}\033[m')
+        return header(f'>>>>> \033[1;33m{a} ÷ {b}\033[m = \033[1;34m{res}\033[m')
 
 def exp(a=0, b=0):
     res = a ** b
-    return header(f'>>>>> \033[1;33m{a}^{b}\033[m = \033[1;34m{res:.0f}\033[m')
+    return header(f'>>>>> \033[1;33m{a}^{b}\033[m = \033[1;34m{res}\033[m')
 
 def sqrtt(a=0):
     res = sqrt(a)
@@ -37,4 +37,4 @@ def sqrtt(a=0):
 
 def fac(a):
     res = factorial(a)
-    return header(f'>>>>> \033[1;33m{a}!\033[m = \033[1;34m{res:.0f}\033[m')
+    return header(f'>>>>> \033[1;33m{a}!\033[m = \033[1;34m{res}\033[m')
